cbd4cc7690ea567be3950f973c52f262f6074556
Roman Shamov <deadsher@mail.ru>
2023-05-05 21:06:58 +0500

    Добавляет оптимизированную графику

--- source/js/burger.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector(".navigation__toggle");
-const burgerMenu = document.querySelector(".navigation__list");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("navigation__toggle--active")
-  burger.classList.toggle("navigation__toggle--not-active");
-  burgerMenu.classList.toggle("navigation__list--active");
-})

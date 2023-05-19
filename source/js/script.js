67c23253d56cdb2fa3783566968c8dd99920c775
Roman Shamov <deadsher@mail.ru>
2023-05-19 21:53:59 +0500

    Редактирует все для защиты получается

--- source/js/script.js
+++ source/js/script.js
@@ -10,19 +10,3 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("navigation__toggle--not-active");
   menu.classList.toggle("navigation__list--active");
 })
-
-
-// Slider
-
-let sliderControl = document.getElementsByClassName("slider__control")[0];
-sliderControl.addEventListener("input", () => {
-  slide();
-})
-
-function slide() {
-  let slideValue = document.getElementsByClassName("slider__control")[0].value;
-  let sublay = document.getElementsByClassName("slider__photo--sublay")[0];
-  let overlay = document.getElementsByClassName("slider__photo--overlay")[0];
-  sublay.style.clipPath = "polygon(0 0, " + slideValue + "% 0, " + slideValue + "% 100%, 0 100%)";
-  overlay.style.clipPath = "polygon(" + slideValue + "% 0, 100% 0, 100% 100%, " + slideValue + "% 100%)";
-}
